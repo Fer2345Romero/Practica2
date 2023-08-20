912b04cce13bfdc862a93fd8cf622623f81aa6ad
Fer2345Romero <140003497+Fer2345Romero@users.noreply.github.com>
2023-08-20 09:40:04 -0600

    Add files via upload

---
+++ WindowsFormsApp3/practica2.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class practica2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMontoInicial = new System.Windows.Forms.TextBox();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMontoInicial
+            // 
+            this.txtMontoInicial.AccessibleName = "MontoInicial";
+            this.txtMontoInicial.Location = new System.Drawing.Point(120, 177);
+            this.txtMontoInicial.Multiline = true;
+            this.txtMontoInicial.Name = "txtMontoInicial";
+            this.txtMontoInicial.Size = new System.Drawing.Size(154, 45);
+            this.txtMontoInicial.TabIndex = 0;
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.AccessibleName = "Porcentaje";
+            this.txtPorcentaje.Location = new System.Drawing.Point(346, 174);
+            this.txtPorcentaje.Multiline = true;
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(201, 36);
+            this.txtPorcentaje.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.AccessibleName = "Button1";
+            this.btnCalcular.Location = new System.Drawing.Point(141, 245);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(106, 35);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AccessibleName = "Resultado";
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(311, 252);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Monto Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(348, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Porcentaje";
+            // 
+            // practica2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.txtMontoInicial);
+            this.Name = "practica2";
+            this.Text = "practica2";
+            this.Load += new System.EventHandler(this.practica2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMontoInicial;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WindowsFormsApp3/practica2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class practica2 : Form
+    {
+        public practica2()
+        {
+            InitializeComponent();
+        }
+        private void practica2_Load(object sender, EventArgs e)
+        {
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double montoInicial = double.Parse(txtMontoInicial.Text);
+                double porcentaje = double.Parse(txtPorcentaje.Text);
+
+                double interesCompuesto = montoInicial + (montoInicial * (porcentaje / 100));
+
+                lblResultado.Text = $"El interés compuesto es: {interesCompuesto:C}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+    }
+}
